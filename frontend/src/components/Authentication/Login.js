0c6e36f628007b5125174b3ea51eacc3298a72c1
Akshay166 <68295230+Akshay166@users.noreply.github.com>
2023-03-28 17:04:31 +0530

    Update Login.js

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
         },
       };
 
-      const { data } = await axios.post("/api/user/login",
+      const { data } = await axios.post("https://ctapp.onrender.com/api/user/login",
         { email, password },
         config
       );
